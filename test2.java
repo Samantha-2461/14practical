50b709228c29607231db21d883c2ad70d3db2834
Samantha-2461 <siwelasamantha49@gmail.com>
2026-03-02 16:02:28 +0200

    Create test2.java

---
+++ test2.java
@@ -0,0 +1,53 @@
+public class openHash {
+    private class Entry {
+        String key;
+        String value;
+        Entry(String k, String v) { key = k; value = v; }
+    }
+    
+    private Entry[] table;
+    private int m;
+    
+    public openHash(int tableSize) {
+        this.m = tableSize;
+        table = new Entry[m + 1]; 
+    }
+    
+    private int hash(String key) {
+        
+        int h = key.hashCode() & 0x7fffffff; 
+        return (h % m) + 1;
+    }
+    
+    public void insert(String key, String value) {
+        int i = hash(key);
+        int start = i;
+        
+        
+        while (table[i] != null) {
+            if (table[i].key.equals(key)) {
+                table[i].value = value; 
+                return;
+            }
+            i = (i % m) + 1; 
+            if (i == start) return; 
+        }
+        
+        
+        table[i] = new Entry(key, value);
+    }
+    
+    public String lookup(String key) {
+        int i = hash(key);
+        int start = i;
+        
+        while (table[i] != null) {
+            if (table[i].key.equals(key)) {
+                return table[i].value;
+            }
+            i = (i % m) + 1;
+            if (i == start) break;
+        }
+        return null;
+    }
+}
