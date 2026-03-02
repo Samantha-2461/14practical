9d3b52aa42c438a7fc225bbaa3624baa6348d58c
Samantha-2461 <siwelasamantha49@gmail.com>
2026-03-02 16:01:01 +0200

    Update test1.java

--- test1.java
+++ test1.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 public class DataGenerator {
     public static class Pair {
-        public String key;  // Make public for access in Timer
+        public String key;  
         public String value;
         public Pair(String k, String v) { key = k; value = v; }
     }
 
     public static Pair[] generateData() {
-        int N = 1 << 20; // 2^20 = 1,048,576
+        int N = 1 << 20; 
         
-        // Create array of keys
+        
         Integer[] keys = new Integer[N];
         for (int i = 0; i < N; i++) keys[i] = i + 1;
         
-        // Shuffle keys
+        
         List<Integer> keyList = Arrays.asList(keys);
         Collections.shuffle(keyList);
         
-        // Create key-value pairs
+        
         Pair[] data = new Pair[N];
         for (int i = 0; i < N; i++) {
             String keyStr = keys[i].toString();
